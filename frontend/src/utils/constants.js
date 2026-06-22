18e32f73c246a901df209cebf116fe4e0cd10241
Abdulsamad25 <100205577+Abdulsamad25@users.noreply.github.com>
2026-06-22 12:50:03 +0100

    created a vercel.json file to ensure files are routed properly.

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api" || "https://invoiceflowapp.vercel.app/api";
 
 export const INVOICE_STATUS = {
   DRAFT: "Draft",
